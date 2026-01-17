4384ca025a86fdb40f7e58c34cd793c2ef21b109
Savindu Keshara <darkscripts.dev@gmail.com>
2026-01-17 23:33:53 +0530

    added ogg files update engine

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.flutter_generative_lofi_engine"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "28.0.13004108"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
